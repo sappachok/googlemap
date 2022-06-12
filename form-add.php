d0718497e4fd9c7c88f2580e3799690e0d7afd06
sappachok <suppachok_sin@nstru.ac.th>
2022-06-12 14:37:13 +0700

    .

---
+++ form-add.php
@@ -0,0 +1,17 @@
+<form action="insert.php" method="post">
+    <p>
+        <label>Name</label><br>        
+        <input type="text" name="name">
+    </p>
+    <p>
+        <label>Latitude</label><br>        
+        <input type="text" name="lat">
+    </p>
+    <p>
+        <label>Longitude</label><br>        
+        <input type="text" name="lon">
+    </p>
+    <p>
+        <button type="submit" name="save" value="true">บันทึกเพิ่มรายการ</button>
+    </p>             
+</form>
