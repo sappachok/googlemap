08b67395035ec7193da970d2f5b2ab633a9b2858
sappachok <suppachok_sin@nstru.ac.th>
2022-06-12 15:07:06 +0700

    .

--- insert.php
+++ insert.php
@@ -1,11 +1,31 @@
 <?php
-    $name = $_POST["name"];
-    $lat = $_POST["lat"];
-    $lon = $_POST["lon"];
-    $save = $_POST["save"];
+    $mysqli = new mysqli("localhost", "root", "", "mygps") or die("Cannot connect database."); 
 
-    echo "<b>เพิ่มรายการ</b><br>";
-    echo "Name: $name<br>";
-    echo "Latitude: $lat<br>";
-    echo "Longitude: $lon<br>";   
+    // Check connection
+    if ($mysqli -> connect_errno) {
+        echo "Failed to connect to MariaDB: " . $mysqli -> connect_error;
+        exit();
+    }
+
+    if(@$_POST) {
+        $name = $_POST["name"];
+        $lat = $_POST["lat"];
+        $lon = $_POST["lon"];
+        $save = $_POST["save"];
+        
+        echo "<b>เพิ่มรายการ</b><br>";
+        echo "Name: $name<br>";
+        echo "Latitude: $lat<br>";
+        echo "Longitude: $lon<br>";
+
+        $sql = "INSERT INTO location (name, lat, lon) 
+            values ('$name','$lat','$lon')";
+
+		$result = $mysqli -> query($sql);
+        if($result) {
+			echo "เพิ่มข้อมูลเสร็จสิ้น<br>";
+			header("Location: index.php");
+		}
+    }
+        
 ?>
