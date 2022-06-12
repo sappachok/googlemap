f26face2eb1a0d299cf245aed65f7e4b9e470cf2
sappachok <suppachok_sin@nstru.ac.th>
2022-06-12 15:00:59 +0700

    .

---
+++ insert.php
@@ -0,0 +1,11 @@
+<?php
+    $name = $_POST["name"];
+    $lat = $_POST["lat"];
+    $lon = $_POST["lon"];
+    $save = $_POST["save"];
+
+    echo "<b>เพิ่มรายการ</b><br>";
+    echo "Name: $name<br>";
+    echo "Latitude: $lat<br>";
+    echo "Longitude: $lon<br>";   
+?>
