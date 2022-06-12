15d03e6469214297f279273228bf63d784ae0617
sappachok <suppachok_sin@nstru.ac.th>
2022-06-12 14:15:04 +0700

    .

--- list.php
+++ list.php
@@ -1,11 +1,15 @@
 <?php
-    $mysqli = new mysqli("localhost", "root", "", "mygps2") or die("Cannot connect database."); 
+    $mysqli = new mysqli("localhost", "root", "", "mygps") or die("Cannot connect database."); 
 
     // Check connection
     if ($mysqli -> connect_errno) {
         echo "Failed to connect to MariaDB: " . $mysqli -> connect_error;
         exit();
     }
-    
-    
+
+    $sql = "SELECT * FROM location ORDER BY id";
+	$result = $mysqli -> query($sql);
+
+    var_dump($result);
+
 ?>
